6a4049aac59108606f5e35e3c0c11bcb8538c2fc
Alex Koller <koller1@live.com>
2016-08-20 16:56:28 -0500

    Restructured project files

---
+++ Ora Challenge/Views/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController.swift
+//  Ora Challenge
+//
+//  Created by Alex Koller on 8/20/16.
+//  Copyright © 2016 Alex Koller. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+}
+
