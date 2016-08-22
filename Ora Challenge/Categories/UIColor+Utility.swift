f02bd653edd93dccdc2fb258e15824037e22b1bf
Alex Koller <koller1@live.com>
2016-08-22 16:41:47 -0500

    Added UIColor category for common colors

---
+++ Ora Challenge/Categories/UIColor+Utility.swift
@@ -0,0 +1,21 @@
+//
+//  UIColor+Utility.swift
+//  Ora Challenge
+//
+//  Created by Alex Koller on 8/22/16.
+//  Copyright © 2016 Alex Koller. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static func oraOrange() -> UIColor {
+        return UIColor(red: 0.98, green: 0.71, blue: 0.18, alpha: 1.00)
+    }
+    
+    static func oraBlack() -> UIColor {
+        return UIColor(red: 0.01, green: 0.01, blue: 0.01, alpha: 1.00)
+    }
+    
+}
